4bdfefb435a7541dae551afea6a746ea4c565cf5
WaffleManse <damiano1994@naver.com>
2025-04-08 14:22:50 +0900

    최고점수 관련 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,11 +13,14 @@ public class GameManager : MonoBehaviour
     public Text nowScore;
     public Text bestScore;
 
+    public Animator anim;
+
     bool isPlay = true;
 
     float time = 0.0f;
 
     string Key = "bestScore";
+    static float MaxScore = 0;
 
     private void Awake()
     {
@@ -30,7 +33,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.DeleteAll();
+        //PlayerPrefs.DeleteAll();
         Time.timeScale= 1.0f;
         InvokeRepeating("MakeSquare", 0f, 1f);
     }
@@ -53,32 +56,45 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         isPlay = false;
-        Time.timeScale = 0.0f;
+        anim.SetBool("isDie", true);
+        Invoke("TimeStop", 0.5f);
         nowScore.text = time.ToString("N2");
 
-        //최고점수가 있다면
-        if(PlayerPrefs.HasKey(Key))
+        if(MaxScore < time)
         {
-            float best = PlayerPrefs.GetFloat(Key);//최고점수 < 현재 점수
-            if (best < time)
-            {
-                PlayerPrefs.SetFloat(Key, time);//현재점수를 최고 점수에 저장
-                bestScore.text = time.ToString("N2");
-            }
-            else
-            {
-                bestScore.text = best.ToString("N2");   //최고점수 > 현재점수
-            }
-        }
-        else//최고점수가 없다면
-        {
-            PlayerPrefs.SetFloat(Key, time);//현재 점수를 저장
+            MaxScore = time;
             bestScore.text = time.ToString("N2");
         }
-            
-            
-        
-        endPanel.SetActive(true);
+        else
+        {
+            bestScore.text = MaxScore.ToString("N2");
+        }
 
+        //최고점수가 있다면
+        //if(PlayerPrefs.HasKey(Key))
+        //{
+        //    float best = PlayerPrefs.GetFloat(Key);//최고점수 < 현재 점수
+        //    if (best < time)
+        //    {
+        //        PlayerPrefs.SetFloat(Key, time);//현재점수를 최고 점수에 저장
+        //        bestScore.text = time.ToString("N2");
+        //    }
+        //    else
+        //    {
+        //        bestScore.text = best.ToString("N2");   //최고점수 > 현재점수
+        //    }
+        //}
+        //else//최고점수가 없다면
+        //{
+        //    PlayerPrefs.SetFloat(Key, time);//현재 점수를 저장
+        //    bestScore.text = time.ToString("N2");
+        //}
+                  
+        endPanel.SetActive(true);
+    }
+
+    void TimeStop()
+    {
+        Time.timeScale = 0.0f;
     }
 }
